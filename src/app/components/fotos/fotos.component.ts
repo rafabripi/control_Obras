4ea58c540710e64ebd41c1031de4fa628a5a64e6
Rafael Bribiesca P <rafabripi@gmail.com>
2019-01-23 15:56:01 -0600

    pipe de imagen, falta autorization

--- src/app/components/fotos/fotos.component.ts
+++ src/app/components/fotos/fotos.component.ts
@@ -12,20 +12,30 @@ export class FotosComponent implements OnInit {
   checklist: string;
   obraId: any;
   fotos: Img[];
+  fotoItem: any;
 
   constructor(private _fotosService: FotosService) {
-    this.checklist = 'anticipo';
+    this.checklist = 'estimacion-2';
     this.obraId = '5c1c2be74be5a91a30e25d72';
+    this.fotos = [];
   }
 
   ngOnInit() {
     let data = {checklist: this.checklist, obraId: this.obraId};
     this._fotosService.getImgs(data)
       .subscribe(resp => {
-        this.fotos = resp[0]['nombre'];
-        let url = (URL_SERVICES + '/img/getImgs/' + this.fotos);
-        console.log(url);
+        this.fotos = resp;
+        this.fotoItem = this.fotos[0]['nombre'];
+        // console.log(fotoItem, 'hallooooooooooooooooooooooooooooooooooooooooooooooo');
+        // hasta aqui el arreglo de fotos se guarda en this.fotos
+        // en las siguientes lineas se hara un prueba de imprimir una foto
       });
+
+      // this._fotosService.getImg(fotoItem)
+      // .subscribe(respFoto => {
+      //     this.foto = respFoto;
+      //   });
+
   }
 
 }
--- src/app/pipes/imagen.pipe.ts
+++ src/app/pipes/imagen.pipe.ts
@@ -1,12 +1,17 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { URL_SERVICES } from '../config/config';
 
 @Pipe({
   name: 'imagen'
 })
 export class ImagenPipe implements PipeTransform {
 
-  transform(value: any, args?: any): any {
-    return null;
+  transform(nombre: string): any {
+    let url = URL_SERVICES + `/img/getImg/?nombre=${nombre}`;
+    if (!nombre) {
+      return url = 'retornar_url_no_valida_para_regresar_img_default';
+    }
+    return url;
   }
 
 }
